d4bf710c25c36334e4d72badb43dd0e8ca236214
Harshawardhan Kamble <kambleharsh08@outlook.com>
2023-10-06 15:45:16 +0530

    Login button added

--- Episode 6/src/Components/Header.js
+++ Episode 6/src/Components/Header.js
@@ -1,5 +1,14 @@
 import logo from "../utils/logo.png";
+import { useState } from "react";
 const Header = () => {
+  const [log, setLog] = useState("Login");
+  const toggleLog = () => {
+    if (log == "Login") {
+      setLog("Logout");
+    } else {
+      setLog("Login");
+    }
+  };
   return (
     <div className="header">
       <div className="logo-container">
@@ -11,7 +20,9 @@ const Header = () => {
           <li>About</li>
           <li>Contact us</li>
           <li>Cart</li>
-          <button className="login-btn">Login</button>
+          <button className="login-btn" onClick={toggleLog}>
+            {log}
+          </button>
         </ul>
       </div>
     </div>
