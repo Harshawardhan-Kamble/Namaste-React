2611c8ef8eb5362b025ccae4af8037a78df0a387
Harshawardhan Kamble <kambleharsh08@outlook.com>
2023-10-07 17:52:41 +0530

    search functionality added

--- Episode 6/src/Components/Body.js
+++ Episode 6/src/Components/Body.js
@@ -1,23 +1,10 @@
 import RestaurantList from "./RestaurantList";
 import { useState } from "react";
-// in this we have passed the function topRated from parent to child just how you pass props from parent to child
-const Body = () => {
-  const [showTopRated, setShowTopRated] = useState(false);
-  const topRated = () => {
-    setShowTopRated(true);
-  };
 
+const Body = () => {
   return (
     <div className="body">
-      {/* <div className="search">Search</div> */}
-      <div className="filter">
-        <button className="filter-btn" onClick={topRated}>
-          Rating 4.0+
-        </button>
-      </div>
-      <div className="res-container">
-        <RestaurantList showTopRated={showTopRated} />
-      </div>
+      <RestaurantList />
     </div>
   );
 };
