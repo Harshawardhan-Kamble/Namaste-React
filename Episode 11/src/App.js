c170429ac8994642c1cfa9693d41daf87fae8385
Harshawardhan Kamble <kambleharsh08@outlook.com>
2023-10-13 13:48:46 +0530

    style added

---
+++ Episode 11/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Header from "./Components/Header";
+import Body from "./Components/Body";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Error from "./Components/Error";
+import ContactUs from "./Components/ContactUs";
+import RestMenu from "./Components/RestMenu";
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Outlet />
+      <Footer />
+    </React.Fragment>
+  );
+};
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contactus",
+        element: <ContactUs />,
+      },
+      {
+        path: "/restaurant/:id",
+        element: <RestMenu />,
+      },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
---
+++ Episode 11/src/Components/Header.js
@@ -0,0 +1,40 @@
+import logo from "../../assets/logo.png";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
+const Header = () => {
+  const [log, setLog] = useState("Login");
+  const toggleLog = () => {
+    setLog(log === "Login" ? "Logout" : "Login");
+  };
+  const onlineStatus = useOnlineStatus();
+
+  return (
+    <div className="header ">
+      <div className="logo-container">
+        <img className="logo" src={logo} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>{onlineStatus ? "🟢" : "🛑"}</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contactus">Contact us</Link>
+          </li>
+          <li>Cart</li>
+          <li>
+            <button className="log-btn" onClick={toggleLog}>
+              {log}
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header;
