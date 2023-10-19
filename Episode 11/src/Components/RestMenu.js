9c5c2fc05b6b87aba6ccaef6a7f1013836aeed4a
Harshawardhan Kamble <kambleharsh08@outlook.com>
2023-10-19 23:34:17 +0530

    Menu categories added

--- Episode 11/src/Components/RestMenu.js
+++ Episode 11/src/Components/RestMenu.js
@@ -8,15 +8,17 @@ const RestMenu = () => {
   if (resInfo === null) return <Shimmer />;
   const { name, areaName, costForTwoMessage, cuisines, avgRating, feeDetails } =
     resInfo?.cards[0]?.card?.card?.info;
-
+// console.log(resInfo)
   // console.log(
-  //   resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card
+  //   resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards
   // );
-  const { itemCards } =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+  // const { itemCards } =
+  //   resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
   // const { itemCard2 } =
   //   resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card
   //     .itemCards;
+  const categories=resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((category)=>category?.card?.card?.["@type"]==='type.googleapis.com/swiggy.presentation.food.v2.ItemCategory')
+  console.log(categories)
 
   return (
     <div>
@@ -27,7 +29,7 @@ const RestMenu = () => {
       <p>{avgRating}</p>
       <p>{feeDetails.message}</p>
       <h2>Menu</h2>
-      <h4>{itemCards[0].card.info.category}</h4>
+      {/* <h4>{itemCards[0].card.info.category}</h4>
       <ul>
         {itemCards.map((item, index) => (
           <li key={index}>
@@ -36,7 +38,17 @@ const RestMenu = () => {
               item?.card?.info?.defaultPrice / 100}
           </li>
         ))}
+      </ul> */}
+      <ul>
+        {categories.map((category,index)=>(
+          <li key={index}>
+            <h4>{category?.card?.card?.title}</h4>
+            {category?.card?.card?.itemCards.map((item,index)=>(<p key={index}>{item?.card?.info?.name}</p>))}
+
+          </li>
+        ))}
       </ul>
+
     </div>
   );
 };
