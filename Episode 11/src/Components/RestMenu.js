ec8f626f58cd044d43b4beedd15ac4b5bcb502d5
Harshawardhan Kamble <kambleharsh08@outlook.com>
2023-10-20 16:34:45 +0530

    Accordian header

--- Episode 11/src/Components/RestMenu.js
+++ Episode 11/src/Components/RestMenu.js
@@ -1,4 +1,5 @@
 import useRestaurantMenu from "../utils/useRestuarantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 const RestMenu = () => {
@@ -6,9 +7,9 @@ const RestMenu = () => {
   // Used useRestaurantMenu custom hook
   const resInfo = useRestaurantMenu(id);
   if (resInfo === null) return <Shimmer />;
-  const { name, areaName, costForTwoMessage, cuisines, avgRating, feeDetails } =
+  const { name, areaName, costForTwoMessage, cuisines, avgRating, feeDetails ,sla} =
     resInfo?.cards[0]?.card?.card?.info;
-// console.log(resInfo)
+console.log(resInfo)
   // console.log(
   //   resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards
   // );
@@ -23,12 +24,12 @@ const RestMenu = () => {
   return (
     <div className="text-center">
       <h1 className="font-bold my-6 text-2xl text-lightBlack">{name}</h1>
-      <p className="font-bold text-lg">{cuisines.join(",")}</p>
-      <p className="font-bold text-lg">{areaName}</p>
-      <p className="font-bold text-lg">{costForTwoMessage}</p>
+      <p className=" text-sm text-gray-500">{cuisines.join(",")}</p>
+      <p className=" text-sm  text-gray-500">{areaName}</p>
+      <p className="text-sm  text-gray-500 m-3">{feeDetails.message}</p>
+      <span className="font-bold my-6 text-2lg text-lightBlack">{sla.deliveryTime}MINS</span>
+      <span className="font-bold my-6 text-2lg text-lightBlack m-5">{costForTwoMessage}</span>
       <p className="font-bold text-lg">{avgRating}</p>
-      <p className="font-bold text-lg">{feeDetails.message}</p>
-      <h2>Menu</h2>
       {/* <h4>{itemCards[0].card.info.category}</h4>
       <ul>
         {itemCards.map((item, index) => (
@@ -39,15 +40,15 @@ const RestMenu = () => {
           </li>
         ))}
       </ul> */}
-      <ul>
+      {/* <ul> */}
         {categories.map((category,index)=>(
-          <li key={index}>
-            <h4>{category?.card?.card?.title}</h4>
-            {category?.card?.card?.itemCards.map((item,index)=>(<p key={index}>{item?.card?.info?.name}</p>))}
-
-          </li>
+          // <li key={index}>
+          //   <h4>{category?.card?.card?.title}</h4>
+          //   {category?.card?.card?.itemCards.map((item,index)=>(<p key={index}>{item?.card?.info?.name}</p>))}
+<RestaurantCategory data={category?.card?.card}/>
+        //   </li>
         ))}
-      </ul>
+      {/* // </ul> */}
 
     </div>
   );
